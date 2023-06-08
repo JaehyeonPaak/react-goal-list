0db3f5e07c5a389f1d166f04ff5bb124a8771a33
JaehyeonPaak <wogus392@gmail.com>
2023-06-09 08:15:53 +0900

    style: styled components with dynamic props2

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,14 +8,19 @@ import styled from 'styled-components';
 const CourseInput = props => {
   const FormControl = styled.div`
     margin: 0.5rem 0;
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
+
+    & label {
+      font-weight: bold;
+      display: block;
+      margin-bottom: 0.5rem;
+      color: ${props => props.invalid ? 'red' : 'black'};
+    }
 
     & input {
       display: block;
       width: 100%;
       border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+      background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
       font: inherit;
       line-height: 1.5rem;
       padding: 0 0.25rem;
@@ -25,11 +30,6 @@ const CourseInput = props => {
       outline: none;
       background: #fad0ec;
       border-color: #8b005d;
-    }
-
-    &.invalid input {
-      border-color: red;
-      background-color: #ffd7d7;
     }
 
     &.invalid label {
