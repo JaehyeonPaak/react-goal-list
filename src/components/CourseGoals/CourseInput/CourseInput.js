bd5f1c0d20f28264b9c266c78fffa42a4a8105ea
wogus392 <wogus392@nexol.co.kr>
2023-06-09 10:24:01 +0900

    fix: fixing bugs

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,22 +5,21 @@ import './CourseInput.css';
 
 import styled from 'styled-components';
 
-const CourseInput = props => {
-  const FormControl = styled.div`
+const FormControl = styled.div`
     margin: 0.5rem 0;
 
     & label {
       font-weight: bold;
       display: block;
       margin-bottom: 0.5rem;
-      color: ${props => props.invalid ? 'red' : 'black'};
+      color: ${props => (props.invalid ? 'red' : 'black')};
     }
 
     & input {
       display: block;
       width: 100%;
       border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
-      background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
+      background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
       font: inherit;
       line-height: 1.5rem;
       padding: 0 0.25rem;
@@ -37,6 +36,7 @@ const CourseInput = props => {
     }
   `;
 
+const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
 
